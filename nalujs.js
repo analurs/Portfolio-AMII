bd5d952a84e1a167a96e9bbdb119592f01051054
Ana Ribeiro <analugrs@gmail.com>
2018-06-12 20:31:29 -0300

    meh

---
+++ nalujs.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.isso').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        arrows: true
+    })
+});
